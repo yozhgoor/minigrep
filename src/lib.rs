b2511a76403cf80a4d62ff407d19324022294e6c
yozhgoor <yozhgoor@outlook.com>
2021-07-23 17:42:25 +0200

    Add a test on the program logic

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+            Rust:
+            safe, fast, productive.
+            Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
