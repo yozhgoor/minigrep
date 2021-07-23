64aec14d4f1ac6458dc2a2d39c8bff8eadb93381
yozhgoor <yozhgoor@outlook.com>
2021-07-23 17:50:28 +0200

    Adapt tests to be able to search with case or not

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,9 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
@@ -43,13 +45,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
             Rust:
             safe, fast, productive.
-            Pick three.";
+            Pick three.
+            Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+            Rust:
+            safe, fast, productive.
+            Pick three.
+            Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
